e489a04ca32a34657967f656f796ec699a8939ed
Pradeep Sathyamurthy <pradeepindepaul@gmail.com>
2017-05-26 09:55:22 -0500

    Web Scrapping
    Web Scrapping Using BeautifulSoup

--- course3/week-4/Assignment4.2.py
+++ course3/week-4/Assignment4.2.py
@@ -2,19 +2,34 @@
 """
 Created on Fri May 26 00:52:02 2017
 
-@author: prade
+@author: pradeep sathyamurthy
 """
 
 import urllib.request as url
 from bs4 import BeautifulSoup as bs
-url_link = "http://python-data.dr-chuck.net/known_by_Fikret.html"
-url_conn = url.urlopen(url_link)
-url_raw_data = url_conn.read()
-#print(url_raw_data)
-url_soup = bs(url_raw_data,"html.parser")
-a_link = url_soup('a')
-#print(a_link)
-for item in a_link:
-    #print(item)
-    name=item.text
-    print(name)
+#url_link = "http://python-data.dr-chuck.net/known_by_Fikret.html"
+url_link = input('Enter URL:')
+position = input('Enter position:')
+position = int(position)
+count = input('Enter count:')
+count = int(count)
+counter=1
+while True:
+    if(counter==count+2):
+        break
+    else:
+        #print('counter>>>>',counter)
+        print('Retrieving:', url_link,'\n')
+        url_conn = url.urlopen(url_link)
+        url_raw_data = url_conn.read()
+        #print(url_raw_data)
+        url_soup = bs(url_raw_data,"html.parser")
+        a_link = url_soup('a')
+        positions_link = a_link[position-1]
+        #print(positions_link)
+        final_url = positions_link.get('href',None)
+        #print(final_url)
+        url_link = final_url
+        counter=counter+1
+        continue
+#print(final_url)
